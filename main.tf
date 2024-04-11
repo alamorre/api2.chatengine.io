df0dfca931fde3b56e71233564b4cee357ce04dc
Adam La Morre <alamorre@gmail.com>
2024-04-11 12:20:09 -0700

    working V1

--- main.tf
+++ main.tf
@@ -1,67 +1,54 @@
-# Define AWS provider and set the region for resource provisioning
-provider "aws" {
-  region = "us-east-1"
-}
+# Creating an ECS cluster
+resource "aws_ecs_cluster" "cluster" {
+  name = "cluster"
 
-# Create a Virtual Private Cloud to isolate the infrastructure
-resource "aws_vpc" "default" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "CE_VPC"
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
   }
 }
 
-# Internet Gateway to allow internet access to the VPC
-resource "aws_internet_gateway" "default" {
-  vpc_id = aws_vpc.default.id
-  tags = {
-    Name = "CE_IG"
-  }
+# Creating an ECS task definition
+resource "aws_ecs_task_definition" "task" {
+  family                   = "service"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE", "EC2"]
+  cpu                      = 512
+  memory                   = 2048
+
+  container_definitions = jsonencode([
+    {
+      name : "nginx",
+      image : "nginx:1.23.1",
+      cpu : 512,
+      memory : 2048,
+      essential : true,
+      portMappings : [
+        {
+          containerPort : 80,
+          hostPort : 80,
+        },
+      ],
+    },
+  ])
 }
 
-# Route table for controlling traffic leaving the VPC
-resource "aws_route_table" "default" {
-  vpc_id = aws_vpc.default.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.default.id
-  }
-  tags = {
-    Name = "CE_RT"
-  }
-}
+# Creating an ECS service
+resource "aws_ecs_service" "service" {
+  name             = "service"
+  cluster          = aws_ecs_cluster.cluster.id
+  task_definition  = aws_ecs_task_definition.task.arn
+  desired_count    = 1
+  launch_type      = "FARGATE"
+  platform_version = "LATEST"
 
-# Subnet within VPC for resource allocation, in availability zone us-east-1a
-resource "aws_subnet" "subnet1" {
-  vpc_id                  = aws_vpc.default.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true # give public IP addresses to instances in this subnet
-  availability_zone       = "us-east-1a"
-  tags = {
-    Name = "CE_Subnet_1"
+  network_configuration {
+    assign_public_ip = true
+    security_groups  = [aws_security_group.ecs_sg.id]
+    subnets          = [aws_subnet.public_subnet.id]
   }
-}
 
-# Another subnet for redundancy, in availability zone us-east-1b
-resource "aws_subnet" "subnet2" {
-  vpc_id                  = aws_vpc.default.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = false
-  availability_zone       = "us-east-1b"
-  tags = {
-    Name = "CE_Subnet_2"
+  lifecycle {
+    ignore_changes = [task_definition]
   }
-}
-
-# Associate subnets with route table for internet access
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.subnet1.id
-  route_table_id = aws_route_table.default.id
-}
-
-resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.subnet2.id
-  route_table_id = aws_route_table.default.id
 }

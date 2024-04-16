b22dd8a67b2f892b103e349f251f9a5be374bc6e
Adam La Morre <alamorre@gmail.com>
2024-04-16 07:41:47 -0700

    Comment everything

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,61 +1,61 @@
-resource "aws_security_group" "alb_sg" {
-  name        = "ce-alb-sg"
-  description = "Security group for ALB"
-  vpc_id      = aws_vpc.main.id
+# resource "aws_security_group" "alb_sg" {
+#   name        = "ce-alb-sg"
+#   description = "Security group for ALB"
+#   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "HTTPS"
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     description = "Allow all outbound traffic"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-resource "aws_lb" "app_lb" {
-  name               = "ce-app-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
+# resource "aws_lb" "app_lb" {
+#   name               = "ce-app-lb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.alb_sg.id]
+#   subnets            = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
 
-  enable_deletion_protection = false # TODO: Set to true for production
-}
+#   enable_deletion_protection = false # TODO: Set to true for production
+# }
 
-resource "aws_lb_target_group" "app_tg" {
-  name     = "app-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+# resource "aws_lb_target_group" "app_tg" {
+#   name     = "app-tg"
+#   port     = 80
+#   protocol = "HTTP"
+#   vpc_id   = aws_vpc.main.id
 
-  health_check {
-    protocol            = "HTTP"
-    path                = "/health"
-    matcher             = "200"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 5
-    unhealthy_threshold = 2
-  }
-}
+#   health_check {
+#     protocol            = "HTTP"
+#     path                = "/health"
+#     matcher             = "200"
+#     interval            = 30
+#     timeout             = 5
+#     healthy_threshold   = 5
+#     unhealthy_threshold = 2
+#   }
+# }
 
-resource "aws_lb_listener" "https_listener" {
-  load_balancer_arn = aws_lb.app_lb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.cert.arn
+# resource "aws_lb_listener" "https_listener" {
+#   load_balancer_arn = aws_lb.app_lb.arn
+#   port              = 443
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   certificate_arn   = aws_acm_certificate.cert.arn
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.app_tg.arn
+#   }
+# }
--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,113 +1,113 @@
-resource "aws_security_group" "ecs_sg" {
-  name        = "ce-ecs-sg"
-  description = "Security group for ECS cluster"
-  vpc_id      = aws_vpc.main.id
+# resource "aws_security_group" "ecs_sg" {
+#   name        = "ce-ecs-sg"
+#   description = "Security group for ECS cluster"
+#   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    description = "Allow HTTP traffic"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "Allow HTTP traffic"
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     description = "Allow all outbound traffic"
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-# IAM role for ECS task execution
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ce_ecs_task_execution_role"
+# # IAM role for ECS task execution
+# resource "aws_iam_role" "ecs_task_execution_role" {
+#   name = "ce_ecs_task_execution_role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        },
-        Effect = "Allow",
-      },
-    ],
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole",
+#         Principal = {
+#           Service = "ecs-tasks.amazonaws.com"
+#         },
+#         Effect = "Allow",
+#       },
+#     ],
+#   })
+# }
 
-# Attach the Amazon ECS task execution policy to the role
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
+# # Attach the Amazon ECS task execution policy to the role
+# resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+#   role       = aws_iam_role.ecs_task_execution_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+# }
 
-# Creating an ECS cluster
-resource "aws_ecs_cluster" "cluster" {
-  name = "ce-http-cluster"
+# # Creating an ECS cluster
+# resource "aws_ecs_cluster" "cluster" {
+#   name = "ce-http-cluster"
 
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
-  }
-}
+#   setting {
+#     name  = "containerInsights"
+#     value = "enabled"
+#   }
+# }
 
-# Creating an ECS task definition
-resource "aws_ecs_task_definition" "task" {
-  family                   = "service"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE", "EC2"]
-  cpu                      = 256 # Match container_definitions
-  memory                   = 512 # Match container_definitions
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+# # Creating an ECS task definition
+# resource "aws_ecs_task_definition" "task" {
+#   family                   = "service"
+#   network_mode             = "awsvpc"
+#   requires_compatibilities = ["FARGATE", "EC2"]
+#   cpu                      = 256 # Match container_definitions
+#   memory                   = 512 # Match container_definitions
+#   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
-  container_definitions = jsonencode([
-    {
-      name : "apichatengine",
-      image : "620457613573.dkr.ecr.us-east-1.amazonaws.com/apichatengine:latest",
-      cpu : 256,
-      memory : 512,
-      essential : true,
-      portMappings : [
-        {
-          containerPort : 80,
-          hostPort : 80,
-        },
-      ],
-    },
-  ])
-}
+#   container_definitions = jsonencode([
+#     {
+#       name : "apichatengine",
+#       image : "620457613573.dkr.ecr.us-east-1.amazonaws.com/apichatengine:latest",
+#       cpu : 256,
+#       memory : 512,
+#       essential : true,
+#       portMappings : [
+#         {
+#           containerPort : 80,
+#           hostPort : 80,
+#         },
+#       ],
+#     },
+#   ])
+# }
 
-# Creating an ECS service
-resource "aws_ecs_service" "service" {
-  name                 = "ce-http-service"
-  cluster              = aws_ecs_cluster.cluster.id
-  task_definition      = aws_ecs_task_definition.task.arn
-  desired_count        = 1
-  launch_type          = "FARGATE"
-  platform_version     = "LATEST"
-  force_new_deployment = true # Forces a new deployment with each apply
+# # Creating an ECS service
+# resource "aws_ecs_service" "service" {
+#   name             = "ce-http-service"
+#   cluster          = aws_ecs_cluster.cluster.id
+#   task_definition  = aws_ecs_task_definition.task.arn
+#   desired_count    = 1
+#   launch_type      = "FARGATE"
+#   platform_version = "LATEST"
+#   # force_new_deployment = true # Forces a new deployment with each apply
 
 
-  network_configuration {
-    assign_public_ip = true
-    security_groups  = [aws_security_group.ecs_sg.id]
-    subnets          = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
-  }
+#   network_configuration {
+#     assign_public_ip = true
+#     security_groups  = [aws_security_group.ecs_sg.id]
+#     subnets          = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
+#   }
 
-  lifecycle {
-    ignore_changes = [task_definition]
-  }
+#   lifecycle {
+#     ignore_changes = [task_definition]
+#   }
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.app_tg.arn
-    container_name   = "apichatengine"
-    container_port   = 80
-  }
+#   load_balancer {
+#     target_group_arn = aws_lb_target_group.app_tg.arn
+#     container_name   = "apichatengine"
+#     container_port   = 80
+#   }
 
-  depends_on = [
-    aws_lb_listener.https_listener,
-  ]
-}
+#   depends_on = [
+#     aws_lb_listener.https_listener,
+#   ]
+# }

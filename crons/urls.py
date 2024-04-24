5ef7657913b967c2ba587fe2be87b9d6f6a8f45f
Adam La Morre <alamorre@gmail.com>
2024-04-24 16:09:09 -0700

    Final patch on tests

--- crons/urls.py
+++ crons/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     re_path(r'^apply_chat_updates$', views.ApplyChatUpdates.as_view()),
     re_path(r'^sync_member_ids$', views.SyncMemberIDs.as_view()),
     re_path(r'^prune_biz_chat$', views.PruneBusinessChat.as_view()),
-    re_path(r'^receive_buffer$', views.ReceiveBuffer.as_view()),
     re_path(r'^owner_to_admin$', views.OwnerToAdmin.as_view()),
     re_path(r'^business_accounts$', views.BusinessAccounts.as_view()),
     re_path(r'^end_trials/(?P<days>[0-9]+)/$', views.EndTrialsCron.as_view()),

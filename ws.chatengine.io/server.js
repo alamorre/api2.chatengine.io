13ac58a14c90f6100288a9e34cc2ea687aaab0b1
Adam La Morre <alamorre@gmail.com>
2024-04-29 18:30:12 -0700

    Server is listening

---
+++ ws.chatengine.io/server.js
@@ -0,0 +1,26 @@
+const uWS = require("uWebSockets.js");
+
+const port = 9001;
+
+uWS
+  .App()
+  .ws("/*", {
+    /* WebSocket behavior */
+    open: (ws) => {
+      console.log("A WebSocket connected via URL: " + ws.url);
+    },
+    message: (ws, message, isBinary) => {
+      /* Echo the message back */
+      ws.send(message, isBinary);
+    },
+    close: (ws, code, message) => {
+      console.log("WebSocket closed");
+    },
+  })
+  .listen(port, (token) => {
+    if (token) {
+      console.log(`WebSocket server listening on port ${port}`);
+    } else {
+      console.log("Failed to listen on port " + port);
+    }
+  });

62ce8d591f78518f49949d1a090ebda7565c1fb7
Adam La Morre <alamorre@gmail.com>
2024-05-12 12:32:11 -0700

    Basic auth with session token

--- api.chatengine.io/users/urls.py
+++ api.chatengine.io/users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     re_path(r'^me/session/$', views.MySession.as_view()),
     re_path(r'^$', views.PeoplePrivateApi.as_view()),
     re_path(r'^search/$', views.SearchOtherUsers.as_view()),
+    re_path(r'^session_auth/(?P<session_token>.+)/$', views.SessionTokenAuth.as_view()),
     re_path(r'^(?P<person_id>[0-9]+)/$', views.PersonPrivateApi.as_view()),
 ]

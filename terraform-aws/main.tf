17c7aa5bf3f4f6873ff68d4c75a64e2b475be46b
Adam La Morre <alamorre@gmail.com>
2024-05-14 17:02:50 -0700

    both http and https listeners

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -76,7 +76,7 @@ resource "aws_security_group" "http_sg" {
   ingress {
     description = "Allow HTTP inbound traffic"
     from_port   = 80
-    to_port     = 80
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # Allows access from any IP
   }
@@ -210,12 +210,21 @@ resource "aws_lb_target_group" "nginx_tg" {
 
 resource "aws_lb_listener" "nginx_listener" {
   load_balancer_arn = aws_lb.nginx_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.api_cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx_tg.arn
+  }
+}
+
+resource "aws_lb_listener" "nginx_listener" {
+  load_balancer_arn = aws_lb.nginx_alb.arn
   port              = 80
   protocol          = "HTTP"
-  # port            = 443
-  # protocol        = "HTTPS"
-  # ssl_policy      = "ELBSecurityPolicy-2016-08"
-  # certificate_arn = aws_acm_certificate.api_cert.arn
 
   default_action {
     type             = "forward"

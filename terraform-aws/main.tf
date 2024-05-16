394cfa2bfad33e62a17f8265707fafa83429adaa
Adam La Morre <alamorre@gmail.com>
2024-05-16 14:59:08 -0700

    Redis is now up and running too

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -147,6 +147,16 @@ resource "aws_ecs_task_definition" "nginx" {
           protocol      = "tcp"
         }
       ]
+      environment = [
+        {
+          name  = "REDIS_HOST"
+          value = aws_elasticache_cluster.redis_cluster.cache_nodes[0].address
+        },
+        {
+          name  = "REDIS_PORT"
+          value = tostring(aws_elasticache_cluster.redis_cluster.port)
+        }
+      ]
     }
   ])
 }
@@ -288,4 +298,30 @@ resource "aws_route53_record" "api_dns" {
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_ecr" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+# Elasticache on Redis
+
+resource "aws_elasticache_subnet_group" "redis_subnet_group" {
+  name       = "redis-subnet-group"
+  subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+
+  tags = {
+    Name = "redis-subnet-group"
+  }
+}
+
+resource "aws_elasticache_cluster" "redis_cluster" {
+  cluster_id           = "redis-cluster"
+  engine               = "redis"
+  node_type            = "cache.t3.micro" # Adjust as needed
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis7"
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
+  security_group_ids   = [aws_security_group.internal_sg.id]
+
+  tags = {
+    Name = "redis-cluster"
+  }
 }

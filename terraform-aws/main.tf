26e18f8d75f360c8a02ae407bcf1bba4623d7bb6
Adam La Morre <alamorre@gmail.com>
2024-05-16 13:57:35 -0700

    Public IP security in the VPC

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -27,10 +27,10 @@ resource "aws_subnet" "subnet1" {
   vpc_id                  = aws_vpc.ce_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "ce-private-subnet1"
+    Name = "ce-public-subnet1"
   }
 }
 
@@ -38,10 +38,10 @@ resource "aws_subnet" "subnet2" {
   vpc_id                  = aws_vpc.ce_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "ce-private-subnet2"
+    Name = "ce-public-subnet2"
   }
 }
 
@@ -104,7 +104,7 @@ resource "aws_security_group" "internal_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Allows access from any IP
+    cidr_blocks = [aws_vpc.ce_vpc.cidr_block] # restrict to aws_vpc IP ranges
   }
 
   egress {
@@ -184,7 +184,7 @@ resource "aws_ecs_service" "nginx_service" {
   network_configuration {
     subnets          = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
     security_groups  = [aws_security_group.internal_sg.id]
-    assign_public_ip = false
+    assign_public_ip = true
   }
 
   load_balancer {

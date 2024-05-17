073b58d259ae81990afd9ae965f4ed9015d0a1b4
Adam La Morre <alamorre@gmail.com>
2024-05-17 16:47:29 -0700

    route53.tf file

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -3,43 +3,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Route53 and SSL
-
-resource "aws_acm_certificate" "api_cert" {
-  domain_name       = var.domain_name
-  validation_method = "DNS"
-  tags = {
-    Environment = "production"
-  }
-}
-
-resource "aws_route53_record" "api_cert_validation" {
-  name    = tolist(aws_acm_certificate.api_cert.domain_validation_options)[0].resource_record_name
-  type    = tolist(aws_acm_certificate.api_cert.domain_validation_options)[0].resource_record_type
-  records = [tolist(aws_acm_certificate.api_cert.domain_validation_options)[0].resource_record_value]
-  ttl     = 60
-
-  zone_id = var.zone_id
-}
-
-resource "aws_acm_certificate_validation" "api_cert_validation" {
-  certificate_arn         = aws_acm_certificate.api_cert.arn
-  validation_record_fqdns = [aws_route53_record.api_cert_validation.fqdn]
-}
-
-resource "aws_route53_record" "api_dns" {
-  zone_id = var.zone_id
-  name    = var.domain_name
-  type    = "A"
-
-  alias {
-    # dualstack prefix is required for ALB
-    name                   = "dualstack.${aws_lb.ce_alb.dns_name}"
-    zone_id                = aws_lb.ce_alb.zone_id
-    evaluate_target_health = true
-  }
-}
-
 # ECR and permissions
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_ecr" {

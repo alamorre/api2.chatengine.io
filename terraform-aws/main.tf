0a7295af20cefd905965601602edbe3e0f0a6725
Adam La Morre <alamorre@gmail.com>
2024-05-17 16:48:38 -0700

    elasticache.tf file

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -2,29 +2,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-# Elasticache on Redis
-
-resource "aws_elasticache_subnet_group" "redis_subnet_group" {
-  name       = "redis-subnet-group"
-  subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
-
-  tags = {
-    Name = "redis-subnet-group"
-  }
-}
-
-resource "aws_elasticache_cluster" "redis_cluster" {
-  cluster_id           = "redis-cluster"
-  engine               = "redis"
-  node_type            = "cache.t3.micro" # Adjust as needed
-  num_cache_nodes      = 1
-  parameter_group_name = "default.redis7"
-  port                 = 6379
-  subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
-  security_group_ids   = [aws_security_group.internal_sg.id]
-
-  tags = {
-    Name = "redis-cluster"
-  }
-}

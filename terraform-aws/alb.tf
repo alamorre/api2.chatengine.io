6084226010abe6145e56a74e2fd3007bb8519ea1
Adam La Morre <alamorre@gmail.com>
2024-05-21 15:05:12 -0700

    Ready for ws connection too

--- terraform-aws/alb.tf
+++ terraform-aws/alb.tf
@@ -33,6 +33,29 @@ resource "aws_lb_target_group" "ce_api_tg" {
   }
 }
 
+resource "aws_lb_target_group" "ce_person_tg" {
+  name        = "person-target-group"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.ce_vpc.id
+  target_type = "ip" # Specify target type as IP
+
+  health_check {
+    path                = "/health/"
+    port                = "8080"
+    protocol            = "HTTP"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "person-target-group"
+  }
+}
+
 resource "aws_lb_listener" "ce_api_listener" {
   load_balancer_arn = aws_lb.ce_alb.arn
   port              = 443
@@ -43,5 +66,39 @@ resource "aws_lb_listener" "ce_api_listener" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.ce_api_tg.arn
+  }
+}
+
+# rule to route /person/* to person-target-group
+resource "aws_lb_listener_rule" "ce_person_listener_rule" {
+  listener_arn = aws_lb_listener.ce_api_listener.arn
+  priority     = 90
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ce_person_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/person_v4/*", "/chat/*"]
+    }
+  }
+}
+
+# rule to route /* to apichatengine-target-group
+resource "aws_lb_listener_rule" "ce_api_listener_rule" {
+  listener_arn = aws_lb_listener.ce_api_listener.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ce_api_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
   }
 }

6aa78b2f681ab39458f425ab27687f8f41ac6df1
Adam La Morre <alamorre@gmail.com>
2024-06-09 11:03:23 -0400

    Changes applied. Setting up new service

--- terraform/aws/elasticache.tf
+++ terraform/aws/elasticache.tf
@@ -7,6 +7,30 @@ resource "aws_elasticache_subnet_group" "redis_subnet_group" {
   }
 }
 
+# Security group for allowing all traffic to and from everywhere
+resource "aws_security_group" "redis_sg" {
+  name        = "redis_sg"
+  description = "Allow traffic to Redis"
+  vpc_id      = aws_vpc.ce_vpc.id
+
+  ingress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # TODO Allow traffic from within the VPC
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "redis-sg"
+  }
+}
+
 resource "aws_elasticache_cluster" "redis_cluster" {
   cluster_id           = "redis-cluster"
   engine               = "redis"
@@ -15,7 +39,7 @@ resource "aws_elasticache_cluster" "redis_cluster" {
   parameter_group_name = "default.redis7"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
-  security_group_ids   = [aws_security_group.internal_sg.id]
+  security_group_ids   = [aws_security_group.redis_sg.id]
 
   tags = {
     Name = "redis-cluster"
